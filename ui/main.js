9915dada1c61849669c68ad828346621fa9f0723
Irfan508 <mirfan508@gmail.com>
2016-09-26 11:52:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+
+//change the text in main.html
 var element=document.getElementById('main-text');
 element.innerHTML="Irfan is my name";
+
+//move the image in the main.html
+var img=document.getElementId("madi");
+img.onclick=function(){
+img.style.marginleft("100px");    
+};
+
