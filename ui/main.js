40ba13c5b56e62c260a5dcf65b6a0336a7b4e5ca
Irfan508 <mirfan508@gmail.com>
2016-09-26 12:04:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,13 @@ element.innerHTML="Irfan is my name";
 
 //move the image in the main.html
 var img=document.getElementId("madi");
+function marginright(){
+    var marginleft=0;
+    marinleft=marginleft+10;
+    img.style.marginleft=marginleft+'px';
+}
 img.onclick=function(){
-img.style.marginleft("100px");    
+    //img.style.marginleft("100px");    
+    var interval=setInterval(moveright,100);
 };
 
